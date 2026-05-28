be2025f25dd3953d7633fe72163a1fb721818c78
Alexander Sedletsky <rasenkaidankabutomushi@gmail.com>
2026-05-28 23:56:25 +0300

    mf

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Birthday
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name']
+
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,64 @@
-from django.shortcuts import render
+from typing import Any
+from datetime import date, datetime
+
+from .forms import BirthdayForm
+from .models import Birthday
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
+from django.urls import reverse_lazy
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+def calculate_birthday_countdown(birthday_str: str) -> int:
+    birthday = datetime.strptime(str(birthday_str), "%Y-%m-%d").date()
+    today = date.today()
+    
+    next_birthday = birthday.replace(year=today.year)
+    
+    if next_birthday < today:
+        next_birthday = next_birthday.replace(year=today.year + 1)
+    
+    return (next_birthday - today).days
+
+
+class BirthdayMixin:
+    model = Birthday
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = "birthday/birthday.html"
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = "id"
+    paginate_by = 10
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+    template_name_suffix = "_detail"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["birthday_countdown"] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
